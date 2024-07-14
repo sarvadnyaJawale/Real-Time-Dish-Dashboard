a0f041bed39197da981bf7b50848857911e5ab46
Sarvadnya Jawale <127713222+sarvadnyaJawale@users.noreply.github.com>
2024-07-14 19:43:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import DishList from './components/DishList';
-import 'C:/Users/sarva/Desktop/dish-dashboard/src/App.css';
+import './App.css';
 
 const App = () => {
   return (
